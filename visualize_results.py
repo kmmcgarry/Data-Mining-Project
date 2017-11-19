2038fdfe524c5a8c4eb0032710297cd802b2695c
Kristen <kristenmcgarry1@gmail.com>
2017-11-19 16:33:21 -0500

    additional image

--- visualize_results.py
+++ visualize_results.py
@@ -23,13 +23,13 @@ plot = df.plot(kind = 'density', title = 'accuracy of inception_v3', legend = Fa
 plot.set_xlabel('accuracy')
 plot.set_ylabel('frequency')
 
-plt.savefig('density.jpg')
+#plt.savefig('density.jpg')
 
 plot_2 = df.plot(kind = 'hist', title = 'accuracy of inception_v3', legend = False, xlim = (0,1.0))
 plot_2.set_xlabel('accuracy')
 plot_2.set_ylabel('frequency')
 
-plt.savefig('histogram.jpg')
+#plt.savefig('histogram.jpg')
 
 f = open('incorrect_analysis.txt')
 results = f.read()
@@ -46,7 +46,7 @@ for x in temp:
 df = pd.DataFrame(pairs, columns = ['class', 'incorrect_class'])
 
 df.groupby(df['class']).count().plot(kind = 'bar', title = 'incorrect classifications by animal', legend = False)
-plt.savefig('incorrect_classes.jpg')
+#plt.savefig('incorrect_classes.jpg')
 
 wild = df[df['class'] == 'wildebeest']
 incorrect = wild.groupby(['incorrect_class']).count().plot(kind = 'bar', title = 'incorrect classifications')
@@ -55,4 +55,5 @@ rhino = df[df['class'] == 'rhinoceros']
 rhino.groupby(['incorrect_class']).count().plot(kind = 'bar', ax = incorrect, color = 'red')
 
 incorrect.legend(labels = ['wildebeest', 'rhinoceros'])
-plt.savefig('incorrect_classes_stacked.jpg')
+plt.tight_layout()
+plt.savefig('incorrect_classes_stacked_3.jpg')
